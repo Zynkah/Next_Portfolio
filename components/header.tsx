66dbdca6117a4675a055d3624453f624d767424a
Zynkah <ffgurl07@yahoo.com>
2024-04-17 13:40:05 -0500

    Fix: size and weight

--- components/header.tsx
+++ components/header.tsx
@@ -2,9 +2,9 @@ import { ModeToggle } from "./mode-toggle";
 
 export default function Header() {
   return (
-    <div className="z-10 fixed top-0 right-0 w-full bg-slate-500 py-12">
+    <div className="z-10 fixed top-0 right-0 w-full bg-slate-500 py-8">
       <div className="flex flex-row justify-center items-center mx-24 text-xl">
-        <span className="mr-auto">Zena Creps</span>
+        <span className="mr-auto text-7xl text-white font-thin">Zena Creps</span>
         <ModeToggle />
       </div>
     </div>
